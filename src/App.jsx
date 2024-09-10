2da4c74f861f631b4334d9f44d42307663475807
CodeForgeNet <codeforgenet@icloud.com>
2024-09-10 14:17:57 +0530

    add Hello file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import './App.css'
+import HelloWorld from './HelloWorld'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <HelloWorld/>
     </>
   )
 }
