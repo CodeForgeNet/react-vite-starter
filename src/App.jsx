35c79cd53767d319ceb384421efe42a1b718916d
CodeForgeNet <codeforgenet@icloud.com>
2024-09-10 15:11:56 +0530

    hlo wrld commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import HelloWorld from './HelloWorld'
 
 function App() {
-
+  
   return (
     <>
       <HelloWorld/>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 // This code is responsible for running top level code :
