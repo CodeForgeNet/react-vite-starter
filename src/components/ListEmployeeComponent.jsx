a5a204c232f48a44d9c4867e99fd84e8dd88b438
CodeForgeNet <codeforgenet@icloud.com>
2024-09-11 10:34:36 +0530

    add componenets

---
+++ src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const ListEmployeeComponent = () => {
+
+    const dummyDate = [
+        {
+            "id": "1",
+            "firstName": "Karan",
+            "lastName": "Singh",
+            "email": "karan@gmail.com"
+        },
+        {
+            "id": "2",
+            "firstName": "Arjun",
+            "lastName": "Bhardwaj",
+            "email": "arjun@gmail.com"
+        },
+        {
+            "id": "3",
+            "firstName": "Krishna",
+            "lastName": "Vasudev",
+            "email": "krishna@gmail.com"
+        }
+    ]
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ListEmployeeComponent
