f62a3f805e228a06c978232c4afb316bb6fa2153
CodeForgeNet <codeforgenet@icloud.com>
2024-09-27 18:17:46 +0530

    add footer component

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const FooterComponent = () => {
   return (
     <div>
-      <footer className='' >
-
+      <footer className='footer' >
+        
       </footer>
     </div>
   )
