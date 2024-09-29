1a23a49a98fdb3f9bc43d96432ec2453f4dbfa7d
CodeForgeNet <codeforgenet@icloud.com>
2024-09-30 00:49:31 +0530

    update appJsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
-import { BrowserRouter, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
@@ -11,7 +11,10 @@ function App() {
     <>
       <BrowserRouter>
         <HeaderComponent />
-        <ListEmployeeComponent />
+        <Routes>
+          // http://localhost:300
+          <Route path='/' element={<ListEmployeeComponent />} ></Route>
+        </Routes>
         <FooterComponent />
       </BrowserRouter>
     </>
