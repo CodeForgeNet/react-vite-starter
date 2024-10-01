bdc0c68bd2d2bfd4d9c0bc08460578430fcac4a3
CodeForgeNet <codeforgenet@icloud.com>
2024-10-01 10:40:04 +0530

    update empCmpnt

--- src/components/EmployeeComponent.jsx
+++ src/components/EmployeeComponent.jsx
@@ -8,18 +8,60 @@ const EmployeeComponent = () => {
   const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
 
+ const [errors, setErrors] = useState({
+    firstName:'',
+    lastName:'',
+    email:''
+  })
+
   const navigator = useNavigate();
 
   function saveEmployee(e) {
     e.preventDefault();
 
+    if(validateForm){
     const employee = { firstName, lastName, email }
     console.log(employee)
 
     createEmployee(employee).then((response) => {
-      console.log(response.data);
-      navigator('/employees')
+    console.log(response.data);
+    navigator('/employees')
     })
+    }
+
+    
+  }
+
+  function validateForm(){
+    let valid = true;
+
+    const errorsCopy = {... errors}
+
+    if(firstName.trim()){
+       errorsCopy.firstName='';
+    }else{
+      errorsCopy.firstName='First name is required';
+      valid = false;
+    }
+
+    if(firstName.trim()){
+      errorsCopy.lastName='';
+    }else{
+      errorsCopy.lastName='Last name is required'
+      valid = false;
+    }
+
+    if(email.trim()){
+      errorsCopy.email='';
+    }else{
+      errorsCopy.email='Email is required'
+      valid = false;
+    }
+
+    setErrors(errorsCopy);
+
+    return valid;
+
   }
 
   return (
